ce4a9265345d0f394adfabca4dbc4cf0a5c5014f
Ji Won Park <vazywon@hotmail.com>
2019-09-20 15:46:38 -0700

    added calculation page

--- screens/CalculationScreen.js
+++ screens/CalculationScreen.js
@@ -1,16 +1,92 @@
-import React, { Component } from 'react';
-import { Button } from 'react-native';
+import React, { Component, Fragment } from 'react';
+import { StyleSheet, Button, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+  },
+  result: {
+    flex: 1,
+    backgroundColor : "red",
+    alignItems : "flex-end"
+  },
+  calculation: {
+    flex: 2,
+    backgroundColor : "green",
+    alignItems : "flex-end"
+  },
+  buttons: {
+    flex:7,
+    flexDirection: "row",
+    backgroundColor: "black"
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor : "yellow"
+  },
+  operations: {
+    flex:1,
+    backgroundColor : "black",
+    justifyContent : "space-around",
+    alignItems: "stretch",
+    
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: "center"
+  }
+})
 
 class CalculationScreen extends Component {
+    
     static navigationOptions = {
-      title: 'Unit',
+        header: null,
     };
     render() {
       const {navigate} = this.props.navigation;
+      const name = this.props.navigation.state.params.name
       return (
-        <Button
-          title="Go to Jane's profile"
-        />
+        <View style={styles.container}>
+          <Button onPress={() => navigate('Home')} title="<<"/>
+          <View style={styles.result}>
+            <Text>6974</Text>
+          </View>
+          <View style={styles.calculation}>
+            <Text>6974</Text>
+          </View>
+          <View style={styles.buttons}>
+            <View style={styles.numbers}>
+              <View style={styles.row}>
+                <Button title="7"/>
+                <Button title="8"/>
+                <Button title="9"/>
+              </View>
+              <View style={styles.row}>
+                <Button title="4"/>
+                <Button title="5"/>
+                <Button title="6"/>
+              </View>
+              <View style={styles.row}>
+                <Button title="1"/>
+                <Button title="2"/>
+                <Button title="3"/>
+              </View>
+              <View style={styles.row}>
+                <Button title="0"/>
+                <Button title="AC"/>
+                <Button title="."/>
+              </View>
+            </View>
+            <View style={styles.operations}>
+                <Button title="/"/>
+                <Button title="x"/>
+                <Button title="-"/>
+                <Button title="+"/>
+            </View>
+          </View>
+        </View>
       );
     }
   }
